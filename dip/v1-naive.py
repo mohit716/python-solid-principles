7f95b0c72d44dd77de3a833fbd4f4625001f2992
Mohit Sharma <200160784+mohit716@users.noreply.github.com>
2026-02-23 19:59:07 -0500

    Add: DI naive v-1

---
+++ dip/v1-naive.py
@@ -0,0 +1,31 @@
+# copy characters from keyboard to printer 
+
+import sys
+
+class Keyboard:
+    def read(self):
+        # read 1 character from stdin (keyboard)
+        c = sys.stdin.read(1)
+        if c == "":   # EOF in Python comes as empty string
+            return none
+        return c
+
+class Printer:
+    def write(self, c):
+        # Write 1 charater to stdout (pretend this is a printer device)
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+def main():
+    keyboard = Keyboard() # hard-coded dependency
+    printer = Printer()  # hard-coded dependency
+
+    while True:
+        c = keyboard.read()  # directly depends on keyboard
+        if c is None:  # EOF
+            break
+        printer.write(c)
+
+if __name__ == "__main__":
+    main()
+
