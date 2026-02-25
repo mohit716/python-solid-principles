7253d72d3d109b37991a22742dd958869d52aeda
Mohit Sharma <200160784+mohit716@users.noreply.github.com>
2026-02-25 00:00:06 -0500

    naive v-2

--- dip/v1-naive.py
+++ dip/v1-naive.py
@@ -1,7 +1,7 @@
 # copy characters from keyboard to printer 
 
 import sys
-
+"""THIS CLASS READS USING SYS.STDIN.READ A CHARACTER  AND CHECK IF ITS EMPTY AND RETURNS NONE OR TYPED LETTER"""
 class Keyboard:
     def read(self):
         # read 1 character from stdin (keyboard)
@@ -9,15 +9,24 @@ class Keyboard:
         if c == "":   # EOF in Python comes as empty string
             return none
         return c
-
+"""THIS CLASS WRITE THE RECEIVED CHARACTER AND LFUSH IT TO MAYBE CLEAR MEMORy"""
 class Printer:
     def write(self, c):
         # Write 1 charater to stdout (pretend this is a printer device)
         sys.stdout.write(c)
         sys.stdout.flush()
 
+class Doer abstract:
+    read()
+    write()
+    :
+class Creater:
+    Doer doer = new Doer
+
 def main():
+    # keyboard object created
     keyboard = Keyboard() # hard-coded dependency
+    # printer object created
     printer = Printer()  # hard-coded dependency
 
     while True:
